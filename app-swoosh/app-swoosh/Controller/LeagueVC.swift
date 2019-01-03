dc3a27f04f95cbd9114061fe73e0eb6e358d0266
Joseph Bielas <jbielas@gmail.com>
2019-01-03 06:17:42 -0800

    Segues programmatically

---
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,35 @@
+//
+//  LeagueVCViewController.swift
+//  app-swoosh
+//
+//  Created by Joseph Bielas on 1/2/19.
+//  Copyright © 2019 DevMettle. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+        
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Joseph Bielas on 12/20/18.
+//  Copyright © 2018 DevMettle. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue){
+        
+    }
+}
+
