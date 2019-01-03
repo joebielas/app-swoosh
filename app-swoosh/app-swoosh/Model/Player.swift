ad24e580696ac6ab4963966a4be159b878400f93
Joseph Bielas <jbielas@gmail.com>
2019-01-03 06:57:24 -0800

    Added the data model

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Joseph Bielas on 1/3/19.
+//  Copyright © 2019 DevMettle. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
